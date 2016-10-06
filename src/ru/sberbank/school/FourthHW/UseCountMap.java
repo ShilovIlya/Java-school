15d9ed5d723eaa4765847986b1a545d3efbbc4cb
Ilya <ShilovIlya@gmail.com>
2016-10-06 13:03:18 +0300

    lesson 4

---
+++ src/ru/sberbank/school/FourthHW/UseCountMap.java
@@ -0,0 +1,19 @@
+package ru.sberbank.school.FourthHW;
+
+public class UseCountMap {
+    public static void main(String[] args) {
+        //пример использования
+        CountMap<Integer> map = new CountMapIml<>();
+
+        map.add(10);
+        map.add(10);
+        map.add(5);
+        map.add(6);
+        map.add(5);
+        map.add(10);
+        System.out.println(map.getCount(5));  // 2
+        System.out.println(map.getCount(6));  // 1
+        System.out.println(map.getCount(10));  //
+
+    }
+}
